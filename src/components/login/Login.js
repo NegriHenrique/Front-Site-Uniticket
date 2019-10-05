8805f6f27a7e42fbb567689d453e586abe1faae2
henriquenegri <henrique@aiqfome.com>
2019-10-05 18:43:59 -0300

    comeco da arquitetura e header

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../css/App.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <h3>Teste</h3>
+    </div>
+  );
+}
+
+export default Login;
